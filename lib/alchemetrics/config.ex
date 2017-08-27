960d25b2ab9267b8d28c1679fa41ed6f964514ce
bernardolins <bernardolins28@gmail.com>
2017-08-27 14:18:11 -0300

    Create Alchemetrics.Config module to handle metrics configuration

---
+++ lib/alchemetrics/config.ex
@@ -0,0 +1,45 @@
+defmodule Alchemetrics.Config do
+  alias Alchemetrics.Exometer.Group
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    case Application.get_env(:alchemetrics, :config_agent) do
+      nil -> nil
+      config_agent -> config_agent.load
+    end
+    {:ok, :loaded}
+  end
+
+  defmacro __using__(_) do
+    quote do
+      import Alchemetrics.Config
+      def load()
+      defoverridable [load: 0]
+    end
+  end
+
+  defmacro group(group_name, do: block) do
+    quote do
+      Group.create_group(%Group{name: unquote(group_name)})
+
+      var!(group_name, Alchemetrics.Config) = unquote(group_name)
+      unquote(block)
+    end
+  end
+
+  defmacro set(key, value) do
+    quote do
+      group_name = var!(group_name, Alchemetrics.Config)
+      Group.set(group_name, unquote(key), unquote(value))
+    end
+  end
+
+  defmacro reporter(module, opts \\ []) do
+    quote do
+      :exometer_report.add_reporter(unquote(module), unquote(opts))
+    end
+  end
+end
