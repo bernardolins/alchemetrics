e98e81d227a9cdef44a6db7da6a6d73fe6db1b7f
bernardolins <bernardolins28@gmail.com>
2018-11-08 17:08:12 -0200

    rename channel to group on group tests

--- test/namespace/group_test.exs
+++ test/namespace/group_test.exs
@@ -6,30 +6,30 @@ defmodule Alchemetrics.GroupTest do
 
   describe "#create" do
     @tag capture_log: true
-    test "stop the channel creation if it is already registered" do
+    test "stop the group creation if it is already registered" do
       Process.flag(:trap_exit, true)
       Alchemetrics.Group.create(:default)
       assert {:error, {:already_registered, _}} = Alchemetrics.Group.create(:default)
       assert_receive {:EXIT, _, {:already_registered, _}}
     end
 
-    test "accepts atom channel names" do
+    test "accepts atom group names" do
       assert {:ok, _} = Alchemetrics.Group.create(:default)
     end
 
-    test "accepts string channel names" do
-      assert {:ok, _} = Alchemetrics.Group.create("channel")
+    test "accepts string group names" do
+      assert {:ok, _} = Alchemetrics.Group.create("group")
     end
 
-    test "accepts list channel names" do
+    test "accepts list group names" do
       assert {:ok, _} = Alchemetrics.Group.create([:default, :for, :users])
     end
 
-    test "accepts KeywordList channel names" do
-      assert {:ok, _} = Alchemetrics.Group.create(channel_name: "my_channel")
+    test "accepts KeywordList group names" do
+      assert {:ok, _} = Alchemetrics.Group.create(group_name: "my_group")
     end
 
-    test "accepts integer channel names" do
+    test "accepts integer group names" do
       assert {:ok, _} = Alchemetrics.Group.create(1)
     end
   end
@@ -50,12 +50,12 @@ defmodule Alchemetrics.GroupTest do
       refute_received{:received, [:message1]}
     end
 
-    test "returns not_found if the channel is not registered" do
+    test "returns not_found if the group is not registered" do
       Process.flag(:trap_exit, true)
       assert {:error, :group_not_found} = Alchemetrics.Group.publish(:default, :some_message)
     end
 
-    test "accepts publish messagens when the channel name is an atom" do
+    test "accepts publish messagens when the group name is an atom" do
       {:ok, pid} = Alchemetrics.Group.create(:default)
       TestConsumer.start_link(pid, self())
       Alchemetrics.Group.publish(:default, :message1)
@@ -63,15 +63,15 @@ defmodule Alchemetrics.GroupTest do
       assert_received{:received, [:message1]}
     end
 
-    test "accepts publish messagens when the channel name is a string" do
-      {:ok, pid} = Alchemetrics.Group.create("channel")
+    test "accepts publish messagens when the group name is a string" do
+      {:ok, pid} = Alchemetrics.Group.create("group")
       TestConsumer.start_link(pid, self())
-      Alchemetrics.Group.publish("channel", :message1)
+      Alchemetrics.Group.publish("group", :message1)
       :timer.sleep(100)
       assert_received{:received, [:message1]}
     end
 
-    test "accepts publish messagens when the channel name is a list" do
+    test "accepts publish messagens when the group name is a list" do
       {:ok, pid} = Alchemetrics.Group.create([:default, :for, :users])
       TestConsumer.start_link(pid, self())
       Alchemetrics.Group.publish([:default, :for, :users], :message1)
@@ -79,15 +79,15 @@ defmodule Alchemetrics.GroupTest do
       assert_received{:received, [:message1]}
     end
 
-    test "accepts publish messagens when the channel name is a KeywordList" do
-      {:ok, pid} = Alchemetrics.Group.create(channel_name: "my_channel")
+    test "accepts publish messagens when the group name is a KeywordList" do
+      {:ok, pid} = Alchemetrics.Group.create(group_name: "my_group")
       TestConsumer.start_link(pid, self())
-      Alchemetrics.Group.publish([channel_name: "my_channel"], :message1)
+      Alchemetrics.Group.publish([group_name: "my_group"], :message1)
       :timer.sleep(100)
       assert_received{:received, [:message1]}
     end
 
-    test "accepts publish messagens when the channel name is a number" do
+    test "accepts publish messagens when the group name is a number" do
       {:ok, pid} = Alchemetrics.Group.create(1)
       TestConsumer.start_link(pid, self())
       Alchemetrics.Group.publish(1, :message1)
@@ -97,7 +97,7 @@ defmodule Alchemetrics.GroupTest do
   end
 
   describe "#join" do
-    test "allows a client to join a channel" do
+    test "allows a client to join a group" do
       Alchemetrics.Group.create(:default)
       {:ok, pid} = ValidationMember.create(pid: self())
       assert :ok = Alchemetrics.Group.join(:default, pid)
@@ -105,7 +105,7 @@ defmodule Alchemetrics.GroupTest do
       assert_receive {:handle_message_called, _}
     end
 
-    test "returns error when a client tries do join a channel that does not exist" do
+    test "returns error when a client tries do join a group that does not exist" do
       {:ok, pid} = ValidationMember.create(pid: self())
       assert {:error, :not_found} == Alchemetrics.Group.join(:default, pid)
     end
